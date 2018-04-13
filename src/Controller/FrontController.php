2f2351257a57edd3416507ac4c954fe94fe1dab6
Valentin Commenge <txreplay@gmail.com>
2018-04-13 16:42:54 +0200

    creation routes

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,13 +3,45 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class FrontController extends Controller
 {
 
-    public function index()
+    public function cities()
     {
-        return new Response('Homepage');
+        return new JsonResponse();
     }
+
+    public function city($city_id)
+    {
+        return new JsonResponse();
+    }
+
+    public function cityRandom()
+    {
+        return new JsonResponse();
+    }
+
+    public function counties() {
+        return new JsonResponse();
+    }
+
+    public function county($county_id) {
+        return new JsonResponse();
+    }
+
+    public function states() {
+        return new JsonResponse();
+    }
+
+    public function state($state_id) {
+        return new JsonResponse();
+    }
+
+    public function search($query) {
+        return new JsonResponse();
+    }
+
+
 }
