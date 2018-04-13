0752db2de0ccdc452a91b172c4da7f579df18a07
Valentin Commenge <txreplay@gmail.com>
2018-04-13 14:50:26 +0200

    Base symfony

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class FrontController extends Controller
+{
+
+    public function index()
+    {
+        return new Response('Homepage');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
